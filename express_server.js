b6e51becccebea3b82c840ca8e78022044595e6a
Your Name <you@example.com>
2021-03-16 23:04:50 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -1,54 +1,52 @@
 /* eslint-disable no-undef */
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({extended: false}));
-
-
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const cookie = require('cookie-parser')
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookie());
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-  
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-  
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// all urls are displayed on the main page
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  res.render("urls_index", templateVars);
+});
+
+// new url is created
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies['username']}
+  res.render("urls_new", templateVars);
 });
+
+// new page is shown
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */
-  res.render("urls_show", templateVars)};
-});
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  if (verifyShortUrl(shortURL, urlDatabase)) {
-    const longURL = urlDatabase[shortURL].longURL;
-    res.redirect(longURL);
+  if (verifyShortUrl(shortURL)) {
+    let longURL = urlDatabase[req.params.shortURL];
+    let templateVars = { shortURL: shortURL, longURL: longURL, username: req.cookies['username']
+    };
+    res.render("urls_show", templateVars);
   } else {
-    res.status(404).send('Does not exist');
+    res.send('does not exist');
   }
-  res.redirect(longURL);
 });
