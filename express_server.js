63d07e5d3ded63fa9d314330f63f24d0d5cbfab6
Your Name <you@example.com>
2021-03-16 08:27:51 +0000

    added header

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -18,7 +19,14 @@ app.listen(PORT, () => {
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */
+  res.render("urls_show", templateVars)};
 });
